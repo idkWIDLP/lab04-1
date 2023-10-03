c7d01446a669dd3bc5f6cd1d3e3a464bdac0fc76
mqxon <maksimkol007@mail.ru>
2023-10-03 13:42:17 +0500

    add test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+from lib import count_common_elements
+
+
+def main():
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [3, 4, 5, 6, 7]
+    list3 = [5, 6, 7, 8, 9]
+
+    print("Количество общих элементов:", count_common_elements(list1, list2, list3))
+
+
+
+if __name__ == '__main__':
+    main()
