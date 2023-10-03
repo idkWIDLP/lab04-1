212da9bf1cafb15acf47d2b6c80455368d2ccee7
mqxon <maksimkol007@mail.ru>
2023-10-03 13:45:51 +0500

    Revert "add test.py"
    This reverts commit c7d01446a669dd3bc5f6cd1d3e3a464bdac0fc76.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def main():
     list1 = [1, 2, 3, 4, 5]
     list2 = [3, 4, 5, 6, 7]
     list3 = [5, 6, 7, 8, 9]
-
+    
     print("Количество общих элементов:", count_common_elements(list1, list2, list3))
 
 
